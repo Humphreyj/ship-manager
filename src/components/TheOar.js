b5225d8539018795d0feb84aa428350264afca63
Joshua Humphrey <joshhumphrey1@gmail.com>
2019-02-19 00:14:44 -0600

    removed wind change temporarily

--- src/components/TheOar.js
+++ src/components/TheOar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Inventory from './Inventory'
 import Weaponry from './Weaponry'
 import ShipStats from './ShipStats'
-import WindDirection from './WindDirection'
+
 
 
 export class TheOar extends Component {
@@ -14,7 +14,7 @@ export class TheOar extends Component {
 				<h3>Ship Stats</h3>
 				<br/>
 				<ShipStats />
-				<WindDirection/>
+				
 				
 				<h3>Weaponry</h3>
 				<Weaponry />
